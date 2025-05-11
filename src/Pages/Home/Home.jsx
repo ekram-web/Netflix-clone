215d1f80450c68a9551bea1b845b02490963c2f2
ekram-web <unuse2001@gmail.com>
2025-05-11 06:00:48 -0700

    movie rowlist and video trailer

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,45 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header.jsx";
 import Footer from "../../Components/Footer/Footer.jsx";
-import Banner from '../../Components/Banner/Banner.jsx'
-// import Rowlist from '../../Components/Rows/RowList/Rowlist.jsx'
+import Banner from "../../Components/Banner/Banner.jsx";
+import Rowlist from "../../Components/Rows/RowList/Rowlist.jsx";
 const Home = () => {
   return (
     <div>
       <Header />
-      <Banner/>
-      {/* <Rowlist/> */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <Banner />
+      <Rowlist />
       <Footer />
     </div>
   );
